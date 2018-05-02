90f36d82f8caf52747101dec003fc312872a157b
Robert Calbul <rcalbul2011@alu.uct.cl>
2018-05-02 01:29:13 -0300

    salto

--- Test2D/Assets/Script/Controlador_jugador.cs
+++ Test2D/Assets/Script/Controlador_jugador.cs
@@ -1,12 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Controlador_jugador : MonoBehaviour {
+public class Controlador_jugador : MonoBehaviour
+{
 
     // Use this for initialization
     public float Velocidad = 5f;
     public float VelocidadMaxima = 10f;
+    public float FuerzaSalto = 8f;
+    public bool EnSuelo = true;
+    public Transform comprobadorSuelo;
+    float comprobadorRadio = 0.07f;
+    public LayerMask MascaraSuelo;
+    public bool DobleSalto = false;
 
     Rigidbody2D Jugador;
     Animator Animador;
@@ -16,23 +21,38 @@ public class Controlador_jugador : MonoBehaviour {
         Jugador = GetComponent<Rigidbody2D>();
         Animador = GetComponent<Animator>();
     }
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((EnSuelo || !DobleSalto) && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jugador.velocity = new Vector2(Jugador.velocity.x, FuerzaSalto);
+            Jugador.AddForce(new Vector2(0, FuerzaSalto));
+            if (!DobleSalto && !EnSuelo)
+            {
+                DobleSalto = true;
+            }
+        }
+    }
 
     private void FixedUpdate()
     {
         float direccion = Input.GetAxis("Horizontal");
         Jugador.AddForce(Vector2.right * Velocidad * direccion);
-
         float LimiteVel = Mathf.Clamp(Jugador.velocity.x, -VelocidadMaxima, VelocidadMaxima);
 
         Jugador.velocity = new Vector2(LimiteVel, Jugador.velocity.y);
+
+        EnSuelo = Physics2D.OverlapCircle(comprobadorSuelo.position, comprobadorRadio, MascaraSuelo);
+        if (EnSuelo)
+        {
+            DobleSalto = false;
+        }
     }
 
 }
